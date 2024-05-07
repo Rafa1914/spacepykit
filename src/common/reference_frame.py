f7a4e20f90616510c86c6e281d6057205f22dd66
Rafael Facchin <rafaelbcfacchin@gmail.com>
2024-05-07 10:38:30 -0300

    Implementation of radec_from_r method

---
+++ src/common/reference_frame.py
@@ -0,0 +1,20 @@
+from src.common.vector import *
+
+def radec_from_r(r : Vector) -> tuple[float,float]:
+    '''
+    Calculates de Right Ascension and Declination for the vector r in its reference frame.
+
+    Atrribute:
+    r: Position vector
+
+    Returns:
+    alpha: Right ascension angle [rad]
+    ddelta: Declination angle [rad]
+    '''
+    cossine_direction_x = r.x/r.magnitude
+    cossine_direction_y = r.y/r.magnitude
+    cossine_direction_z = r.z/r.magnitude
+
+    delta = np.arcsin(cossine_direction_z)
+    alpha = np.arccos(cossine_direction_x/np.cos(delta)) if cossine_direction_y > 0 else 2*np.pi - np.arccos(cossine_direction_x/np.cos(delta))
+    return (alpha,delta)
