0a3b2a3994dfb6ae2823da4e40a49c013daca2a8
Rafael Facchin <rafaelbcfacchin@gmail.com>
2024-05-13 13:37:30 -0300

    Gauss method

--- src/common/constants.py
+++ src/common/constants.py
@@ -1,3 +1,4 @@
 J2000 = 2451545              # Julian day at 01/01/2000 12h (UT)
 EARTH_OBLATENESS = 0.003353  # Oblateness of the Earth [-]
 EARTH_RADIUS = 6378          # Earth radius at equator [km]
+EARTH_GRAVITATIONAL_PARAMETER = 398600 #Earth graviational parameter [km^3/s^2]
