b38d3667e47e6580a0cc0ec36a18654c68daa097
Rafael Facchin <rafaelbcfacchin@gmail.com>
2024-05-28 15:24:36 -0300

    Adding plate-solving

---
+++ src/astrophotometry/match.py
@@ -0,0 +1,139 @@
+from typing import Optional
+
+import numpy as np
+from scipy.spatial import cKDTree
+
+from src.astrophotometry.geometry import get_transform_matrix, pad
+from src.astrophotometry.quads import hashes as hash4
+from src.astrophotometry.triangles import hashes as hash3
+
+
+def count_cross_match(coords1, coords2, tol=1e-3):
+    """
+    Counts the number of cross-matches between two sets of 2D points.
+
+    Parameters
+    ----------
+    coords1 : np.ndarray
+        First set of points coordinates, shape (n, 2).
+    coords2 : np.ndarray
+        Second set of points coordinates, shape (m, 2).
+    tol : float, optional
+        Tolerance of the match, by default 1e-3.
+
+    Returns
+    -------
+    int
+        Number of cross-matches between coords1 and coords2.
+    """
+    return np.count_nonzero(
+        np.min(np.linalg.norm(coords1[:, None, :] - coords2[None, :, :], axis=-1), 0)
+        < tol
+    )
+
+
+def cross_match(coords1, coords2, tolerance=10):
+    """
+    Finds the closest matches between two sets of 2D points.
+
+    Parameters
+    ----------
+    coords1 : np.ndarray
+        First set of points coordinates, shape (n, 2).
+    coords2 : np.ndarray
+        Second set of points coordinates, shape (m, 2).
+    tolerance : float, optional
+        Tolerance of the match, given in coords1 points units, by default 10.
+
+    Returns
+    -------
+    np.ndarray
+        Array of matched indices, where each row contains the indices of the matched points in coords1 and coords2.
+    """
+
+    matches = []
+
+    for i, s in enumerate(coords1):
+        distances = np.linalg.norm(s - coords2, axis=1)
+        closest = np.argmin(distances)
+        if distances[closest] < tolerance:
+            matches.append([i, closest])
+
+    return np.array(matches)
+
+
+def find_transform(
+    radecs: np.ndarray,
+    pixels: np.ndarray,
+    min_match: float = 0.7,
+    asterism: int = 4,
+    quads_tolerance: float = 0.02,
+    tolerance: float = 12,
+) -> np.ndarray:
+    """
+    Finds the transformation matrix from `radecs` to `pixels`.
+
+    Parameters
+    ----------
+    radecs : np.ndarray
+        The coordinates to be transformed, shape (n, 2).
+    pixels : np.ndarray
+        The target coordinates, shape (m, 2).
+    min_match : float, optional
+        The fraction of `pixels` coordinates that must be matched to stop the search.
+        I.e., if the number of matched points is `>= min_match * len(pixels)`, the
+        search stops and return the found transform. By default 0.7.
+    asterism : int, optional
+        The asterism to use for hashing, either 3 or 4, by default 4.
+    quads_tolerance : float, optional
+        The minimum euclidean distance between two quads to be matched and tested.
+        By default 0.02.
+    tolerance : float, optional
+        The minimum distance between two coordinates to be considered cross-matched
+        (in `pixels` units). This serves to compute the number of coordinates being
+        matched between `radecs` and `pixels` for a given transform.
+        By default 12.
+
+    Returns
+    -------
+    np.ndarray
+        The transformation matrix from `radecs` to `pixels`.
+    """
+
+    if asterism == 3:
+        asterism_function = hash3
+    elif asterism == 4:
+        asterism_function = hash4
+    else:
+        raise ValueError("available asterisms are 3 and 4")
+
+    hashes_pixels, asterism_pixels = asterism_function(pixels)
+    hashes_radecs, asterism_radecs = asterism_function(radecs)
+
+    tree_pixels = cKDTree(hashes_pixels)
+    tree_radecs = cKDTree(hashes_radecs)
+    pairs = []
+
+    ball_query = tree_pixels.query_ball_tree(tree_radecs, r=quads_tolerance)
+    matches = []
+
+    for i, j in enumerate(ball_query):
+        if len(j) > 0:
+            pairs += [[i, k] for k in j]
+
+    for i, j in pairs:
+        M = get_transform_matrix(asterism_radecs[j], asterism_pixels[i])
+        test = (M @ pad(radecs).T)[0:2].T
+        match = count_cross_match(pixels, test, tolerance)
+        matches.append(match)
+
+        if min_match is not None:
+            if match >= min_match * len(pixels):
+                break
+
+    if len(matches) == 0:
+        return None
+    else:
+        i, j = pairs[np.argmax(matches)]
+        M = get_transform_matrix(asterism_radecs[j], asterism_pixels[i])
+        return M
---
+++ src/astrophotometry/quads.py
@@ -0,0 +1,87 @@
+from itertools import combinations
+
+import numpy as np
+
+from src.astrophotometry.geometry import proj, u1u2
+
+
+def reorder(quads):
+    distances = np.linalg.norm(
+        quads[:, :, :, None] - np.rollaxis(quads.T, 2)[:, None, :, :], axis=2
+    )
+    i = np.argmax(np.max(distances, 1), 1)
+    idxs = np.roll(
+        np.argsort(distances[range(len(quads)), i], axis=1)[:, ::-1], 1, axis=1
+    )
+    return np.array([q[i] for q, i in zip(quads, idxs)])
+
+
+def good_quads(quads, circletol=0.01):
+    """
+    whether all points are contained in a circle (see Lang2009)
+    """
+    q0, q1 = quads[:, 0], quads[:, 1]
+    r = np.linalg.norm(q1 - q0, axis=1) / 2
+    center = q0 + (q1 - q0) / 2
+    in_circle = np.linalg.norm(quads - center[:, None, :], axis=2) <= r[:, None] * (
+        1 + circletol
+    )
+    return np.all(in_circle, axis=1)
+
+
+def quad_hash(quads):
+    a, b, c, d = np.rollaxis(quads, 1)
+
+    norm = np.linalg.norm(b - a, axis=1)
+
+    def u1u2(a, b):
+        """compute x, y basis as defined in Lang2009"""
+        co_m = np.cos(-np.pi / 4)
+        si_m = np.sin(-np.pi / 4)
+        co_p = np.cos(np.pi / 4)
+        si_p = np.sin(np.pi / 4)
+        rm = np.array([[co_m, -si_m], [si_m, co_m]])
+        rp = np.array([[co_p, -si_p], [si_p, co_p]])
+        x = (rm @ (b - a).T).T + a
+        y = (rp @ (b - a).T).T + a
+        return x, y
+
+    u1, u2 = u1u2(a, b)
+
+    def proj(p, origin, axe):
+        """projection of a point p on a segment from origin to axe"""
+        n = axe - origin
+        n /= np.linalg.norm(n, axis=1)[:, None]
+        return np.sum((p - origin) * n, 1)
+
+    return (
+        np.array(
+            [
+                proj(c, a, u1) / norm,
+                proj(d, a, u1) / norm,
+                proj(c, a, u2) / norm,
+                proj(d, a, u2) / norm,
+            ]
+        ).T,
+        np.rollaxis(np.array([a, b]), 1),
+    )
+
+
+def clean_quads(xy):
+    assert xy.shape[1] == 2
+    quads_idxs = np.array(list(combinations(np.arange(xy.shape[0]), 4)))
+    quads = xy[quads_idxs]
+    ordered_quads = reorder(quads)
+    return ordered_quads[good_quads(ordered_quads)]
+
+
+def hashes(xy):
+    assert xy.shape[1] == 2
+    quads_idxs = np.array(list(combinations(np.arange(xy.shape[0]), 4)))
+    quads = xy[quads_idxs]
+    ordered_quads = reorder(quads)
+    good_ordered_quads = ordered_quads[good_quads(ordered_quads)]
+    h, u = quad_hash(good_ordered_quads)
+    # we sort hashes from larger AB (see Lang 2008)
+    idxs = np.argsort(np.linalg.norm(u[:, 1] - u[:, 0], axis=1))
+    return h[idxs[::-1]], good_ordered_quads[idxs[::-1]]
